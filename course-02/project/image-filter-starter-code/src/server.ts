3ad14aed6a554fbcc99742cca1df27cd8ad7252d
sebastien6 <sebastien6@outlook.com>
2020-02-28 13:29:14 -0500

    added handler /filteredimage

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import {isURL} from 'validator';
+import morgan from 'morgan';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+var timeout = require('connect-timeout');
 
 (async () => {
 
@@ -29,12 +32,49 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
+  app.use(morgan('short'));
+  app.use(timeout('10s'));
+
+  const requireImageUrl = () => {return (req: Request, res: Response, next: function) => {
+      const imageUrl = req.query.image_url;
+
+      if (!imageUrl) {
+        return res.status(400).send({ message: 'Image url is required' });
+      }
+      
+      if (!isURL(imageUrl)) {
+        return res.status(400).send({ message: 'Invalid url format passed as argument' });
+      } else {
+        next();
+      }
+  }};
+
+  // timeout('30s'), haltOnTimedout, 
+  app.get("/filteredimage", requireImageUrl(), async ( req: Request, res: Response ) => {
+    const filteredImagePath: string = await filterImageFromURL(req.query.image_url);
+
+    res.sendFile(filteredImagePath, async(err) => {
+      if(err) {
+        res.status(500).end();
+      }
+      else {
+        console.log('image %s processes successfuly', filteredImagePath)
+      }
+    });
+
+    res.on('finish', () => {
+      deleteLocalFiles([filteredImagePath]);
+      console.log('image %s deleted successfuly', filteredImagePath)
+    })
+  
+ });
   //! END @TODO1
+  
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
+  app.get( "/", async ( req: Request, res: Response ) => {
+    res.status(200).send("try GET /filteredimage?image_url={{}}")
   } );
   
 
